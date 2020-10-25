20d0fc49c701e137f69699027e6aa7ee94942e9a
Viktor Vedmich <victor.vedmich@gmail.com>
2020-10-25 21:27:22 +0300

    add copy logic

--- ubuntu.sh
+++ ubuntu.sh
@@ -21,3 +21,31 @@ cd ~/onedark
 mkdir ~/.vim/colors
 cp colors/onedark.vim ~/.vim/colors/
 cp autoload/onedark.vim ~/.vim/autoload/
+cd ~
+
+# Copy all files
+git pull origin main;
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+function doIt() {
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude ".osx" \
+		--exclude "bootstrap.sh" \
+		--exclude "brew.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE-MIT.txt" \
+		-avh --no-perms . ~;
+	source ~/.bash_profile;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt;
+	fi;
+fi;
+unset doIt;
